fdc3f223893c50813757df387139e4b5d45c979e
Edoardo Biraghi <edoardo.biraghi@gmail.com>
2015-11-09 17:45:44 +0100

    Initialise repository with some basic packages

---
+++ metadata.rb
@@ -0,0 +1,4 @@
+name 				"wordpress_weschool"
+maintainer 			"Tech weschool"
+maintainer_email 	"tecnologia@oilproject.org"
+recipe 				"wordpress_weschool", "Install nginx, varnish, php-fpm and wordpress"
