b18d87ce2598a62672cea3739ebc401da6e35150
Angelo Giuffredi <agiuffredi@gmail.com>
2016-05-23 15:46:29 +0200

    fix

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -7,16 +7,13 @@ deploy 'wordpress' do
   create_dirs_before_symlink.clear
   purge_before_symlink.clear
   symlinks.clear
-  restart_command do
-
-    execute 'php-fpm restart' do
-    	command 'service php5-fpm restart'
-    end
-
-    execute 'clean varnish cache' do
-        command 'varnishadm "ban req.http.host ~ #{node["wordpress_dir"]}"'
-    end
-
-  end
   action :deploy
+end
+
+execute 'php-fpm restart' do
+    command 'service php5-fpm restart'
+end
+
+execute 'clean varnish cache' do
+    command 'varnishadm "ban req.http.host ~ #{node["wordpress_dir"]}"'
 end
